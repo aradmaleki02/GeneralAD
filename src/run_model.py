9ff4958cf7de28093c2fdee37543346402af90ef
Arad Maleki <arad.m.t02@gmail.com>
2024-08-12 03:56:56 +0330

    update

--- src/run_model.py
+++ src/run_model.py
@@ -4,24 +4,13 @@ import sys
 
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
-from pytorch_lightning.loggers import WandbLogger
 
-from .kdad_vit import AD_ViT 
+from .kdad_vit import AD_ViT
 from .simplenet import SimpleNet
 from .general_ad import General_AD
 from .load_data import prepare_loader
 
-import wandb
-
 def run(args):
-    # wandb
-    name = f'{args.wandb_name}_{args.normal_class}_{args.hf_path}'
-    project = f'{args.run_type}_{args.dataset_name}'
-    os.environ["WANDB_API_KEY"] = args.wandb_api_key
-    wandb.login()
-    wandb.init(project=project, entity=args.wandb_entity, name=name)
-    wandb_logger = WandbLogger()
-
     # device
     if not torch.cuda.is_available():
         print("CUDA is not available. Exiting.")
@@ -33,7 +22,6 @@ def run(args):
     # lightning set-up
     trainer = Trainer(
         log_every_n_steps=args.log_every_n_steps,
-        logger=wandb_logger,
         accelerator="gpu",
         devices=1,
         max_epochs=args.epochs,
@@ -47,11 +35,11 @@ def run(args):
     # data loaders
     train_loader, test_loader = prepare_loader(image_size=args.image_size,
                                                         path=args.data_dir,
-                                                        dataset_name=args.dataset_name, 
-                                                        class_name=args.normal_class, 
-                                                        batch_size=args.batch_size, 
+                                                        dataset_name=args.dataset_name,
+                                                        class_name=args.normal_class,
+                                                        batch_size=args.batch_size,
                                                         test_batch_size=args.test_batch_size,
-                                                        num_workers=args.num_workers, 
+                                                        num_workers=args.num_workers,
                                                         seed=args.seed,
                                                         shots=args.shots)
 
@@ -61,7 +49,7 @@ def run(args):
     torch.backends.cudnn.benchmark = False
 
     # train / load model
-    if args.run_type == 'kdad': 
+    if args.run_type == 'kdad':
         if args.load_checkpoint:
             model = AD_ViT.load_from_checkpoint(args.checkpoint_dir)
             checkpoint_dir = args.checkpoint_dir
@@ -121,8 +109,8 @@ def run(args):
                         dsc_heads=args.dsc_heads,
                         dsc_dropout=args.dsc_dropout,
                         pool_size=args.pool_size,
-                        image_size=args.image_size, 
-                        num_fake_patches=args.num_fake_patches, 
+                        image_size=args.image_size,
+                        num_fake_patches=args.num_fake_patches,
                         fake_feature_type=args.fake_feature_type,
                         top_k=args.top_k,
                         log_pixel_metrics=args.log_pixel_metrics,
@@ -139,8 +127,5 @@ def run(args):
     test_result = trainer.test(model, test_loader, verbose=True)
 
     print("Checkpoint directory:", checkpoint_dir)
-
-    # wandb
-    wandb.finish()
 
     return
